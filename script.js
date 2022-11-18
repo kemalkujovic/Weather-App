5b10004fd64ea38ef143e9f78edc88e06ad0ae8d
kemalkujovic <kemalkujovic1111@gmail.com>
2022-11-18 05:37:06 +0100

    Weather-app

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let apiKey = "81c012b926634fca41d6e3b2ad98d52b";
-console.log(apiKey);
 let requestBtn = document.querySelector("#sreachBtn");
+let inputCity = document.querySelector("#inputCity");
+let geoBtn = document.querySelector(".geoBtn");
 requestBtn.addEventListener("click", async function requestApi() {
-  let inputCity = document.querySelector("#inputCity");
   let city = inputCity.value;
-  console.log(city);
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  console.log(api);
   const response = await axios.get(api);
-  console.log(response);
+  displayMessage(response.data);
 });
+
+geoBtn.addEventListener("click", async function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(succesFull, onError);
+  } else {
+    alert("Your browser not support Geo api");
+  }
+});
+async function succesFull(position) {
+  const { latitude, longitude } = position.coords;
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const response = await axios.get(api);
+  console.log(response.data);
+}
+function onError(error) {
+  console.log(error);
+}
+
+function displayMessage(info) {
+  console.log(info);
+}
