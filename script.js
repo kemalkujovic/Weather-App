be5db63e514d4c42c6ac3ab7ca9cfe42dc9ce6e2
kemalkujovic <kemalkujovic1111@gmail.com>
2022-11-18 11:21:15 +0100

    Weather-app

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ let apiKey = "81c012b926634fca41d6e3b2ad98d52b";
 let requestBtn = document.querySelector("#sreachBtn");
 let inputCity = document.querySelector("#inputCity");
 let geoBtn = document.querySelector(".geoBtn");
+let infoText = document.querySelector(".infoText");
 requestBtn.addEventListener("click", async function requestApi() {
   let city = inputCity.value;
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  const response = await axios.get(api);
-  displayMessage(response.data);
+  try {
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const response = await axios.get(api);
+    displayWait(response);
+    displayMessage(response.data);
+  } catch (error) {
+    if (error.request.status === 404) {
+      infoText.innerHTML = `${inputCity.value} it's not valid city`;
+      infoText.classList.add("displayMessageError");
+    }
+  }
 });
-
+async function displayWait(res) {
+  infoText.innerHTML = "Waiting...";
+  infoText.classList.replace("displayMessageError", "displayMessage");
+}
 geoBtn.addEventListener("click", async function () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(succesFull, onError);
@@ -17,15 +29,16 @@ geoBtn.addEventListener("click", async function () {
   }
 });
 async function succesFull(position) {
+  displayWait();
+  infoText.style.display = "block";
   const { latitude, longitude } = position.coords;
   let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
   const response = await axios.get(api);
   displayMessage(response.data);
 }
 function onError(error) {
-  console.log(error);
+  infoText.innerHTML = error.message;
+  infoText.classList.add("displayMessageError");
 }
 
-function displayMessage(info) {
-  console.log(info);
-}
+function displayMessage(info) {}
